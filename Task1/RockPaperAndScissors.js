0f888bb58247c7b2695bf0f1c437eb6509c5e08d
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2025-01-28 09:34:20 +0530

    Add changes to the code

--- Task1/RockPaperAndScissors.js
+++ Task1/RockPaperAndScissors.js
@@ -16,82 +16,108 @@ const askQuestion = (question) => {
 const decideWinner = (player1, player2) => {
     if(player1 == "Rock"){
         if(player2 == "Rock"){
-            console.log("Tie");
+            return "Tie";
         }
         else if(player2 == "Paper"){
-            console.log("Player 2 won");
+            return "Player2";
         }
         else if(player2 == "Scissors"){
-            console.log("Player 1 won");
+            return "Player1";
         }
     }
     else if(player1 == "Paper"){
         if(player2 == "Rock"){
-            console.log("Player 1 won");
+            return "Player1";
         }
         else if(player2 == "Paper"){
-            console.log("Tie");
+            return "Tie";
         }
         else if(player2 == "Scissors"){
-            console.log("Player 2 won");
+            return "Player2";
         }
     }
     else if(player1 == "Scissors"){
         if(player2 == "Rock"){
-            console.log("Player 2 won");
+            return "Player2";
         }
         else if(player2 == "Paper"){
-            console.log("Player 1 won");
+            return "Player1";
         }
         else if(player2 == "Scissors"){
-            console.log("Tie");
+            return "Tie";
         }
     }
 };
 
 const main = async () => {
-  let player1 = "", player2 = "";
 
-  const answer1 = await askQuestion(
-    `
-    Enter 1 for Rock \n
-    Enter 2 for Paper \n
-    Enter 3 for Scissors \n
-    Enter choice of user1 : \n
-    `
-  );
+  let points1 = 0, points2 = 0, round = 1;
 
-  if (answer1 == 1) {
-    player1 = choices[0];
-  } else if (answer1 == 2) {
-    player1 = choices[1];
-  } else {
-    player1 = choices[2];
-  }
+  while(points1 < 5 && points2 < 5){
 
-  const answer2 = await askQuestion(
-    `
-    Enter 1 for Rock \n
-    Enter 2 for Paper \n
-    Enter 3 for Scissors \n
-    Enter choice of user2 : \n
-    `
-  );
+    let player1 = "", player2 = "";
 
-  if (answer2 == 1) {
-    player2 = choices[0];
-  } else if (answer2 == 2) {
-    player2 = choices[1];
-  } else {
-    player2 = choices[2];
+    console.log(`Round ${round}`);
+
+    const answer1 = await askQuestion(
+        `
+        Enter 1 for Rock \n
+        Enter 2 for Paper \n
+        Enter 3 for Scissors \n
+        Enter choice of user1 : \n
+        `
+    );
+
+    if (answer1 == 1) {
+        player1 = choices[0];
+    } else if (answer1 == 2) {
+        player1 = choices[1];
+    } else {
+        player1 = choices[2];
+    }
+
+    const answer2 = await askQuestion(
+        `
+        Enter 1 for Rock \n
+        Enter 2 for Paper \n
+        Enter 3 for Scissors \n
+        Enter choice of user2 : \n
+        `
+    );
+
+    if (answer2 == 1) {
+        player2 = choices[0];
+    } else if (answer2 == 2) {
+        player2 = choices[1];
+    } else {
+        player2 = choices[2];
+    }
+
+    let winner = decideWinner(player1, player2);
+    
+    if(winner == "Tie"){
+
+    }
+    else if(winner == "Player1"){
+        points1++;
+    }
+    else if(winner == "Player2"){
+        points2++;
+    }
+
+    console.log("\n");
+
+    round++;
   }
 
   rl.close();
 
-//   console.log(player1);
-//   console.log(player2);
+  let winner = "";
 
-  decideWinner(player1, player2);
+  if(points1 == 5) winner = "Player 1";
+  else winner = "Player 2";
+
+  console.log(`Winner of the game is : ${winner}`);
 
 };
 
